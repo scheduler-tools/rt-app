6465487f874f2a8ddca4111f32e3d2aabc59e99c
Giacomo Bagnoli <g.bagnoli@asidev.com>
2010-02-03 09:17:18 +0100

    Missing header

---
+++ rt-app.h
@@ -0,0 +1,41 @@
+#ifndef _RT_APP_H_
+#define _RT_APP_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <error.h>
+#include <time.h>
+#include <sched.h>
+#include <pthread.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <libgen.h>
+
+#ifdef AQUOSA
+#define QOS 1000
+#include <aquosa/qres_lib.h>
+#endif /* AQUOSA */
+
+#define PATH_LENGTH 256
+
+
+void *thread_body(void *arg);
+
+struct thread_data {
+	int ind;
+
+	struct timespec min_et, max_et;
+	struct timespec period, deadline;
+    
+    FILE *log_handler;
+    int sched_policy;
+	int sched_prio;
+
+#ifdef AQUOSA
+    int fragment;
+#endif
+};
+
+#endif /* _RT_APP_H_ */
+
