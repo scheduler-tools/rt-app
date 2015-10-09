acbd05df48f43f3e882d0bcf6be5998954da570f
Vincent Guittot <vincent.guittot@linaro.org>
2015-10-09 12:00:48 +0200

    rt-app: fix syscall number definition
    add syscall numbers for aarch64 in libdl
    
    define syscall number in internal libdl header file iff they are not already
    defined by another lib
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

--- libdl/dl_syscalls.h
+++ libdl/dl_syscalls.h
@@ -24,19 +24,43 @@
 #define SCHED_DEADLINE	6
 
 /* XXX use the proper syscall numbers */
+
+/* __NR_sched_setattr number */
+#ifndef __NR_sched_setattr
 #ifdef __x86_64__
 #define __NR_sched_setattr		314
-#define __NR_sched_getattr		315
 #endif
 
 #ifdef __i386__
 #define __NR_sched_setattr		351
-#define __NR_sched_getattr		352
 #endif
 
 #ifdef __arm__
 #define __NR_sched_setattr		380
+#endif
+
+#ifdef __aarch64__
+#define __NR_sched_setattr		274
+#endif
+#endif
+
+/* __NR_sched_getattr number */
+#ifndef __NR_sched_getattr
+#ifdef __x86_64__
+#define __NR_sched_getattr		315
+#endif
+
+#ifdef __i386__
+#define __NR_sched_getattr		352
+#endif
+
+#ifdef __arm__
 #define __NR_sched_getattr		381
+#endif
+
+#ifdef __aarch64__
+#define __NR_sched_getattr		275
+#endif
 #endif
 
 #define SF_SIG_RORUN		2
