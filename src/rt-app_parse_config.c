scheduler-tools/rt-app

commit 17dd99c72a12602ceab30ade5aead5834b8c119d

Clean-up header file inferno
The rt-app header and source files only include C standard library
(glibc), POSIX (threading), json-c header and rt-app header files if
needed.

The autoconf configuration header 'config.h' is included where it is
needed. Currently only the preprocessor symbol DLSCHED is used.

Build tests:

on x86_64 w/o deadline:

$ git reset --hard; git clean -fdx
$ ./autogen.sh
$ ./configure
$ make

on x86_64 w/ deadline:

$ git reset --hard; git clean -fdx
$ ./autogen.sh
$ ./configure --with-deadline
$ make

on arm64 w/ deadline, static

$ git reset --hard; git clean -fdx
$ ./autogen.sh
$ ./configure --host=aarch64-linux-gnu LDFLAGS="--static
-L/opt/git/tools/json-c/.libs/" CFLAGS="-I /opt/git/tools/"
--with-deadline
$ make

Tests ran on arm64 (juno) using static version w/ deadline and a json
file w/ cfs and deadline tasks.

Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

src/rt-app_parse_config.c
@@ -18,7 +18,16 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-#include <json-c/bits.h>
+
+/* for CPU_SET macro */
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <json-c/json.h>
+
+#include "rt-app_utils.h"
 #include "rt-app_parse_config.h"
 
 #define PFX "[json] "

src/rt-app.c
@@ -19,14 +19,23 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+/* for CPU_SET macro */
 #define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
-#include "rt-app.h"
-#include "rt-app_utils.h"
-#include <sched.h>
-#include "pthread.h"
+#include <math.h>
+#include <pthread.h>
+#include <signal.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/mman.h>  /* for memlock */
+
+#include "config.h"
+#include "rt-app_utils.h"
+#include "rt-app_args.h"
 
 static int errno;
 static volatile sig_atomic_t continue_running;

src/rt-app.h
@@ -22,18 +22,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef _RT_APP_H_
 #define _RT_APP_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <error.h>
-#include <time.h>
-#include <sched.h>
-#include <pthread.h>
-#include <signal.h>
-#include <sys/mman.h>  /* for memlock */
-#include "config.h"
-#include "rt-app_types.h"
-#include "rt-app_args.h"
-
 void *thread_body(void *arg);
 
 #endif /* _RT_APP_H_ */

src/rt-app_args.c
@@ -19,7 +19,12 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "rt-app_args.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "rt-app_parse_config.h"
 
 void
 usage (const char* msg, int ex_code)

src/rt-app_args.h
@@ -21,22 +21,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #ifndef _RTAPP_ARGS_H_
 #define _RTAPP_ARGS_H_
-/* for CPU_SET macro */
-#define _GNU_SOURCE
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sched.h>
-#include <getopt.h>
-#include <sys/stat.h>
-#include <string.h>
 #include "rt-app_types.h"
-#include "rt-app_utils.h"
-#ifdef DLSCHED
-#include "dl_syscalls.h"
-#endif
-#include "rt-app_parse_config.h"
 
 void
 usage (const char* msg, int ex_code);

src/rt-app_parse_config.h
@@ -21,23 +21,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #ifndef _RTAPP_PARSE_CONFIG_H
 #define _RTAPP_PARSE_CONFIG_H 
-/* for CPU_SET macro */
-#define _GNU_SOURCE
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sched.h>
-#include <string.h>
 #include "rt-app_types.h"
-#include "rt-app_utils.h"
-#ifdef DLSCHED
-#include "dl_syscalls.h"
-#endif
-#include <json-c/json.h>
 
 void
 parse_config(const char *filename, rtapp_options_t *opts);

src/rt-app_types.h
@@ -22,13 +22,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef _RTAPP_TYPES_H_
 #define _RTAPP_TYPES_H_
 
+#include <pthread.h>
+
 #include "config.h"
 #include "dl_syscalls.h"
-#include <sched.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdio.h>
-#include <sched.h>
 
 #define RTAPP_POLICY_DESCR_LENGTH 16
 #define RTAPP_RESOURCE_DESCR_LENGTH 16

src/rt-app_utils.c
@@ -19,8 +19,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <math.h>
+#include <stdarg.h>
+
 #include "rt-app_utils.h"
 
 unsigned long

commit 7b4ff6dd231f6742c1a75a62b431af64fa6e7a20

Do not use json-c's is_error() any more
In json-c-0.12 is_error() is defined in bits.h which is exported via
json.h. However, json-c's master branch contains commit d4e81f9ec827
which declares bits.h deprecated and removes the export via json.h.

Replace the use of is_error() with its trivial implementation to be
able to get rid of this single dependency towards the deprecated
bits.h.

This fixes an issue introduced by commit 17dd99c72a12 ("Clean-up header
file inferno") which removed #include <json-c/bits.h> from
src/rt-app_parse_config.c.

Cc: Olav Haugan <ohaugan@codeaurora.org>
Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
Acked-by: Vincent Guittot < vincent.guittot@linaro.org>

src/rt-app_parse_config.c
@@ -895,7 +895,7 @@ get_opts_from_json_object(struct json_object *root, rtapp_options_t *opts)
 {
 	struct json_object *global, *tasks, *resources;
 
-	if (is_error(root)) {
+	if (root == NULL) {
 		log_error(PFX "Error while parsing input JSON");
 		exit(EXIT_INV_CONFIG);
 	}