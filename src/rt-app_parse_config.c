scheduler-tools/rt-app

commit 3bda96e6e6641d9409ae21e83dddc57514ca8a35

add phase feature
we can split the thread's behavior into several phases that will behave
differently. This feature is useful to describe a behavior that is not fully
periodic

Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

src/rt-app_parse_config.c
@@ -121,6 +121,7 @@ get_int_value_from(struct json_object *where,
 	set_default_if_needed(key, value, have_def, def_value);
 	assure_type_is(value, where, key, json_type_int);
 	i_value = json_object_get_int(value);
+	json_object_put(value);
 	log_info(PIN "key: %s, value: %d, type <int>", key, i_value);
 	return i_value;
 }
@@ -137,6 +138,7 @@ get_bool_value_from(struct json_object *where,
 	set_default_if_needed(key, value, have_def, def_value);
 	assure_type_is(value, where, key, json_type_boolean);
 	b_value = json_object_get_boolean(value);
+	json_object_put(value);
 	log_info(PIN "key: %s, value: %d, type <bool>", key, b_value);
 	return b_value;
 }
@@ -152,11 +154,13 @@ get_string_value_from(struct json_object *where,
 	value = get_in_object(where, key, have_def);
 	set_default_if_needed_str(key, value, have_def, def_value);
 	if (json_object_is_type(value, json_type_null)) {
+		json_object_put(value);
 		log_info(PIN "key: %s, value: NULL, type <string>", key);
 		return NULL;
 	}
 	assure_type_is(value, where, key, json_type_string);
 	s_value = strdup(json_object_get_string(value));
+	json_object_put(value);
 	log_info(PIN "key: %s, value: %s, type <string>", key, s_value);
 	return s_value;
 }
@@ -315,7 +319,6 @@ serialize_acl(rtapp_resource_access_list_t **acl,
 	if (!(*acl)) {
 		*acl = malloc( sizeof(rtapp_resource_access_list_t));
 		(*acl)->res = &resources[idx];
-//		(*acl)->index = idx;
 		(*acl)->next = NULL;
 		(*acl)->prev = NULL;
 		tmp = *acl;
@@ -331,7 +334,6 @@ serialize_acl(rtapp_resource_access_list_t **acl,
 			/* add the resource to the acl only if it is not already
 			 * present in the list */
 			tmp->next = malloc ( sizeof (rtapp_resource_access_list_t));
-			// tmp->next->index = idx;
 			tmp->next->next = NULL;
 			tmp->next->prev = tmp;
 			tmp->next->res = &resources[idx];
@@ -371,7 +373,7 @@ serialize_acl(rtapp_resource_access_list_t **acl,
 
 static void
 parse_thread_resources(const rtapp_options_t *opts, struct json_object *locks,
-		       struct json_object *task_resources, thread_data_t *data)
+		       struct json_object *task_resources, phase_data_t *data)
 {
 	int i, j, usage_usec;
 	struct json_object *res;
@@ -451,48 +453,36 @@ parse_thread_resources(const rtapp_options_t *opts, struct json_object *locks,
 }
 
 static void
-parse_thread_data(char *name, struct json_object *obj, int idx,
-		  thread_data_t *data, const rtapp_options_t *opts)
+parse_thread_phase_data(struct json_object *obj, int idx,
+		  phase_data_t *data, const rtapp_options_t *opts)
 {
 	long exec, period, dline;
-	char *policy;
-	char def_policy[RTAPP_POLICY_DESCR_LENGTH];
-	struct array_list *cpuset;
-	struct json_object *cpuset_obj, *cpu, *resources, *locks;
-	int i, cpu_idx, prior_def;
-
-	log_info(PFX "Parsing thread %s [%d]", name, idx);
-
-	/* common and defaults */
-	data->ind = idx;
-	data->name = strdup(name);
-	data->lock_pages = opts->lock_pages;
-	data->cpuset = NULL;
-	data->cpuset_str = NULL;
+	struct json_object *resources, *locks;
 
 	/* loop */
 	data->loop = get_int_value_from(obj, "loop", TRUE, -1);
 
-	/* period */
-	period = get_int_value_from(obj, "period", FALSE, 0);
-	if (period <= 0) {
-		log_critical(PIN2 "Cannot set negative period");
-		exit(EXIT_INV_CONFIG);
-	}
-	data->period = usec_to_timespec(period);
-
 	/* exec time */
 	exec = get_int_value_from(obj, "exec", FALSE, 0);
-	if (exec > period) {
-		log_critical(PIN2 "Exec must be greather than period");
-		exit(EXIT_INV_CONFIG);
-	}
 	if (exec < 0) {
 		log_critical(PIN2 "Cannot set negative exec time");
 		exit(EXIT_INV_CONFIG);
 	}
 	data->min_et = usec_to_timespec(exec);
 	data->max_et = usec_to_timespec(exec);
+
+	/* period */
+	period = get_int_value_from(obj, "period", TRUE, exec);
+	if (period <= 0) {
+		log_critical(PIN2 "Cannot set negative period");
+		exit(EXIT_INV_CONFIG);
+	}
+	if (exec > period) {
+		log_critical(PIN2 "Period must be greater or equal than period");
+		exit(EXIT_INV_CONFIG);
+	}
+
+	data->period = usec_to_timespec(period);
 
 	/* deadline */
 	dline = get_int_value_from(obj, "deadline", TRUE, period);
@@ -505,6 +495,47 @@ parse_thread_data(char *name, struct json_object *obj, int idx,
 		exit(EXIT_INV_CONFIG);
 	}
 	data->deadline = usec_to_timespec(dline);
+
+	/* sleep */
+	data->sleep = get_bool_value_from(obj, "sleep", TRUE, 1);
+
+	/* resources */
+	resources = get_in_object(obj, "resources", TRUE);
+	locks = get_in_object(obj, "lock_order", TRUE);
+	if (locks) {
+		assure_type_is(locks, obj, "lock_order", json_type_array);
+		log_info(PIN "key: lock_order %s", json_object_to_json_string(locks));
+		if (resources) {
+			assure_type_is(resources, obj, "resources",
+					json_type_object);
+			log_info(PIN "key: resources %s",
+				  json_object_to_json_string(resources));
+		}
+		parse_thread_resources(opts, locks, resources, data);
+	} else {
+		data->nblockages = 0;
+	}
+
+}
+
+static void
+parse_thread_data(char *name, struct json_object *obj, int idx,
+		  thread_data_t *data, const rtapp_options_t *opts)
+{
+	char *policy;
+	char def_policy[RTAPP_POLICY_DESCR_LENGTH];
+	struct array_list *cpuset;
+	struct json_object *cpuset_obj, *phases_obj, *cpu, *resources, *locks;
+	int i, cpu_idx, prior_def;
+
+	log_info(PFX "Parsing thread %s [%d]", name, idx);
+
+	/* common and defaults */
+	data->ind = idx;
+	data->name = strdup(name);
+	data->lock_pages = opts->lock_pages;
+	data->cpuset = NULL;
+	data->cpuset_str = NULL;
 
 	/* policy */
 	policy_to_string(opts->policy, def_policy);
@@ -529,9 +560,6 @@ parse_thread_data(char *name, struct json_object *obj, int idx,
 	/* delay */
 	data->wait_before_start = get_int_value_from(obj, "delay", TRUE, 0);
 
-	/* sleep */
-	data->sleep = get_bool_value_from(obj, "sleep", TRUE, 1);
-
 	/* cpu set */
 	cpuset_obj = get_in_object(obj, "cpus", TRUE);
 	if (cpuset_obj) {
@@ -551,23 +579,35 @@ parse_thread_data(char *name, struct json_object *obj, int idx,
 	}
 	log_info(PIN "key: cpus %s", data->cpuset_str);
 
-	/* resources */
-	resources = get_in_object(obj, "resources", TRUE);
-	locks = get_in_object(obj, "lock_order", TRUE);
-	if (locks) {
-		assure_type_is(locks, obj, "lock_order", json_type_array);
-		log_info(PIN "key: lock_order %s", json_object_to_json_string(locks));
-		if (resources) {
-			assure_type_is(resources, obj, "resources",
-					json_type_object);
-			log_info(PIN "key: resources %s",
-				  json_object_to_json_string(resources));
-		}
-		parse_thread_resources(opts, locks, resources, data);
-	} else {
-		data->nblockages = 0;
-	}
 
+	data->loop = get_int_value_from(obj, "loop", TRUE, -1);
+
+	/* phases */
+	phases_obj = get_in_object(obj, "phases", TRUE);
+	if (phases_obj) {
+		/* used in the foreach macro */
+		struct lh_entry *entry; char *key; struct json_object *val; int idx;
+
+		assure_type_is(phases_obj, obj, "phases",
+					json_type_object);
+
+		log_info(PIN "Parsing phases section");
+		data->nphases = 0;
+		foreach(phases_obj, entry, key, val, idx) {
+			data->nphases++;
+		}
+		log_info(PIN "Found %d phases", data->nphases);
+		data->phases_data = malloc(sizeof(phase_data_t) * data->nphases);
+		foreach(phases_obj, entry, key, val, idx) {
+			log_info(PIN "Parsing phase %s", key);
+			parse_thread_phase_data(val, idx, &data->phases_data[idx], opts);
+		}
+	} else {
+		data->nphases = 1;
+		data->phases_data = malloc(sizeof(phase_data_t) * data->nphases);
+		data->loop = 1;
+		parse_thread_phase_data(obj, 0, data->phases_data, opts);
+	}
 }
 
 static void
@@ -669,7 +709,7 @@ get_opts_from_json_object(struct json_object *root, rtapp_options_t *opts)
 	parse_resources(resources, opts);
 	json_object_put(resources);
 	parse_tasks(tasks, opts);
-	json_object_put(tasks);
+//	json_object_put(tasks);
 
 }
 

src/rt-app.c
@@ -43,7 +43,7 @@ static ftrace_data_t ft_data = {
 
 /*
  * Function: to do some useless operation.
- *                      TODO: improve the waste loop with more heavy functions
+ * TODO: improve the waste loop with more heavy functions
  */
 void waste_cpu_cycles(int load_loops)
 {
@@ -254,6 +254,7 @@ shutdown(int sig)
 void *thread_body(void *arg)
 {
 	thread_data_t *data = (thread_data_t*) arg;
+	phase_data_t *pdata;
 	struct sched_param param;
 	struct timespec t_now, t_next;
 	unsigned long t_start_usec;
@@ -265,8 +266,7 @@ void *thread_body(void *arg)
 	pid_t tid;
 	struct sched_attr attr;
 	unsigned int flags = 0;
-	int ret, i = 0;
-	int j;
+	int ret, i, j, loop;
 
 	/* set thread name */
 	ret = pthread_setname_np(pthread_self(), data->name);
@@ -274,7 +274,10 @@ void *thread_body(void *arg)
 		perror("pthread_setname_np thread name over 16 characters");
 	}
 
-	/* set thread affinity */
+	/* Get the 1st phase's data */
+	pdata = &data->phases_data[0];
+
+	/* Set thread affinity */
 	if (data->cpuset != NULL)
 	{
 		log_notice("[%d] setting cpu affinity to CPU(s) %s", data->ind,
@@ -306,13 +309,6 @@ void *thread_body(void *arg)
 				exit(EXIT_FAILURE);
 			}
 
-			log_notice("[%d] starting thread with period: %lu, exec: %lu,"
-					"deadline: %lu, priority: %d",
-					data->ind,
-					timespec_to_usec(&data->period),
-					timespec_to_usec(&data->min_et),
-					timespec_to_usec(&data->deadline),
-					data->sched_prio);
 			break;
 
 		case other:
@@ -338,14 +334,6 @@ void *thread_body(void *arg)
 				}
 			}
 
-			log_notice("[%d] starting thread with period: %lu, exec: %lu,"
-					"deadline: %lu, nice: %d",
-					data->ind,
-					timespec_to_usec(&data->period),
-					timespec_to_usec(&data->min_et),
-					timespec_to_usec(&data->deadline),
-					data->sched_prio);
-
 			data->lock_pages = 0; /* forced off for SCHED_OTHER */
 			break;
 
@@ -357,10 +345,10 @@ void *thread_body(void *arg)
 			attr.sched_flags = 0;
 			attr.sched_policy = SCHED_DEADLINE;
 			attr.sched_priority = 0;
-			attr.sched_runtime = timespec_to_nsec(&data->max_et) +
-					(timespec_to_nsec(&data->max_et) /100) * BUDGET_OVERP;
-			attr.sched_deadline = timespec_to_nsec(&data->deadline);
-			attr.sched_period = timespec_to_nsec(&data->period);
+			attr.sched_runtime = timespec_to_nsec(&pdata->max_et) +
+					(timespec_to_nsec(&pdata->max_et) /100) * BUDGET_OVERP;
+			attr.sched_deadline = timespec_to_nsec(&pdata->deadline);
+			attr.sched_period = timespec_to_nsec(&pdata->period);
 		break;
 #endif
 
@@ -382,21 +370,32 @@ void *thread_body(void *arg)
 		}
 	}
 
-	/* if we know the duration we can calculate how many periods we will
+	log_notice("[%d] starting thread with period: %lu, exec: %lu,"
+				"deadline: %lu",
+				data->ind,
+				timespec_to_usec(&pdata->period),
+				timespec_to_usec(&pdata->min_et),
+				timespec_to_usec(&pdata->deadline));
+
+	/*
+	 * If we know the duration we can calculate how many periods we will
 	 * do at most, and log to memory, instead of logging to file.
+	 * In case of several phase, we still use the logging file instead of
+	 * memory.
+	 * TODO: parse all the phase to compute the memory phase
 	 */
 	nperiods = 0;
-	if ((data->duration > 0) && (data->duration > data->wait_before_start)) {
+	if ((data->duration > 0) && (data->duration > data->wait_before_start) && (data->nphases < 2)) {
 		my_duration_usec = (data->duration * 1000000) - data->wait_before_start;
-		nperiods = (my_duration_usec + timespec_to_usec(&data->period) - 1)  / timespec_to_usec(&data->period) + 1;
+		nperiods = (my_duration_usec + timespec_to_usec(&data->phases_data->period) - 1)  / timespec_to_usec(&data->phases_data->period) + 1;
 	}
 
-	if ((data->loop > 0)  && (data->loop < nperiods)) {
+	if ((pdata->loop > 0)  && (pdata->loop < nperiods)) {
 		nperiods = data->loop + 1 ;
 	}
 
 	if (nperiods)
-		timings = malloc ( nperiods * sizeof(timing_point_t));
+		timings = malloc(nperiods * sizeof(timing_point_t));
 	else
 		timings = NULL;
 
@@ -449,21 +448,22 @@ void *thread_body(void *arg)
 
 	clock_gettime(CLOCK_MONOTONIC, &t_now);
 	t_next = t_now;
-	data->deadline = timespec_add(&t_now, &data->deadline);
+	pdata->deadline = timespec_add(&t_now, &pdata->deadline);
 
-	while (continue_running &&  (i != data->loop)) {
+	i = j = loop = 0;
+	while (continue_running && (i != data->loop)) {
 		struct timespec t_start, t_end, t_diff, t_slack;
 
 		if (opts.ftrace)
-			log_ftrace(ft_data.marker_fd, "[%d] begins loop %d", data->ind, i);
+			log_ftrace(ft_data.marker_fd, "[%d] begins loop %d phase %d step %d", data->ind, i, j, loop);
 
 		clock_gettime(CLOCK_MONOTONIC, &t_start);
-		run(data->ind, &data->min_et, &data->max_et, data->blockages,
-		    data->nblockages, data->sleep ? NULL: &t_start);
+		run(data->ind, &pdata->min_et, &pdata->max_et, pdata->blockages,
+		    pdata->nblockages, pdata->sleep ? NULL: &t_start);
 		clock_gettime(CLOCK_MONOTONIC, &t_end);
 
 		t_diff = timespec_sub(&t_end, &t_start);
-		t_slack = timespec_sub(&data->deadline, &t_end);
+		t_slack = timespec_sub(&pdata->deadline, &t_end);
 
 		t_start_usec = timespec_to_usec(&t_start);
 		if (timings)
@@ -472,27 +472,26 @@ void *thread_body(void *arg)
 			curr_timing = &tmp_timing;
 
 		curr_timing->ind = data->ind;
-		curr_timing->period = timespec_to_usec(&data->period);
-		curr_timing->min_et = timespec_to_usec(&data->min_et);
-		curr_timing->max_et = timespec_to_usec(&data->max_et);
+		curr_timing->period = timespec_to_usec(&pdata->period);
+		curr_timing->min_et = timespec_to_usec(&pdata->min_et);
+		curr_timing->max_et = timespec_to_usec(&pdata->max_et);
 		curr_timing->rel_start_time =
 			t_start_usec - timespec_to_usec(&data->main_app_start);
 		curr_timing->abs_start_time = t_start_usec;
 		curr_timing->end_time = timespec_to_usec(&t_end);
-		curr_timing->deadline = timespec_to_usec(&data->deadline);
+		curr_timing->deadline = timespec_to_usec(&pdata->deadline);
 		curr_timing->duration = timespec_to_usec(&t_diff);
 		curr_timing->slack =  timespec_to_lusec(&t_slack);
 
+//		if (!timings)
+//			log_timing(data->log_handler, curr_timing);
 
-		if (!timings)
-			log_timing(data->log_handler, curr_timing);
-
-		t_next = timespec_add(&t_next, &data->period);
-		data->deadline = timespec_add(&data->deadline, &data->period);
+		t_next = timespec_add(&t_next, &pdata->period);
+		pdata->deadline = timespec_add(&pdata->deadline, &pdata->period);
 
 		if (opts.ftrace)
-			log_ftrace(ft_data.marker_fd, "[%d] end loop %d",
-				   data->ind, i);
+			log_ftrace(ft_data.marker_fd, "[%d] end loop %d phase %d step %d",
+				   data->ind, i, j, loop);
 
 		if (curr_timing->slack < 0 && opts.die_on_dmiss) {
 			log_critical("[%d] DEADLINE MISS !!!", data->ind);
@@ -504,10 +503,26 @@ void *thread_body(void *arg)
 		}
 
 		clock_gettime(CLOCK_MONOTONIC, &t_now);
-		if (data->sleep && timespec_lower(&t_now, &t_next))
+		if (pdata->sleep && timespec_lower(&t_now, &t_next))
 			clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t_next, NULL);
 
-		i++;
+		loop++;
+		if (loop == pdata->loop) {
+			loop = 0;
+
+			clock_gettime(CLOCK_MONOTONIC, &t_now);
+			t_next = t_now;
+			pdata->deadline = timespec_add(&t_now, &pdata->deadline);
+
+			j++;
+			if (j == data->nphases) {
+				j = 0;
+
+				i++;
+			}
+
+			pdata = &data->phases_data[j];
+		}
 	}
 
 exit_miss:

src/rt-app_args.c
@@ -86,7 +86,7 @@ parse_thread_args(char *arg, int idx, thread_data_t *tdata, policy_t def_policy)
 			if (period <= 0 )
 				usage("Cannot set negative period.", 
 				      EXIT_INV_COMMANDLINE);
-			tdata->period = usec_to_timespec(period);
+			tdata->phases_data->period = usec_to_timespec(period);
 			i++;
 			break;
 
@@ -99,8 +99,8 @@ parse_thread_args(char *arg, int idx, thread_data_t *tdata, policy_t def_policy)
 			if (exec <= 0 )
 				usage("Cannot set negative exec time",
 						EXIT_INV_COMMANDLINE);
-			tdata->min_et = usec_to_timespec(exec);
-			tdata->max_et = usec_to_timespec(exec);
+			tdata->phases_data->min_et = usec_to_timespec(exec);
+			tdata->phases_data->max_et = usec_to_timespec(exec);
 			i++;
 			break;
 
@@ -152,7 +152,7 @@ parse_thread_args(char *arg, int idx, thread_data_t *tdata, policy_t def_policy)
 			if (dline <= 0 )
 				usage("Cannot set negative deadline",
 				      EXIT_INV_COMMANDLINE);
-			tdata->deadline = usec_to_timespec(dline);
+			tdata->phases_data->deadline = usec_to_timespec(dline);
 			i++;
 			break;
 		}
@@ -165,7 +165,7 @@ parse_thread_args(char *arg, int idx, thread_data_t *tdata, policy_t def_policy)
 	}
 
 	if (dline == 0)
-		tdata->deadline = tdata->period;
+		tdata->phases_data->deadline = tdata->phases_data->period;
 
 	/* set cpu affinity mask */
 	if (tdata->cpuset_str)
@@ -207,6 +207,7 @@ parse_command_line_options(int argc, char **argv, rtapp_options_t *opts)
 	opts->pi_enabled = 0;
 	opts->policy = other;
 	opts->threads_data = malloc(sizeof(thread_data_t));
+	opts->threads_data->phases_data = malloc(sizeof(rtapp_tasks_phases_t));
 
 	static struct option long_options[] = {
 				{"help", 0, 0, 'h'},
@@ -269,6 +270,8 @@ parse_command_line_options(int argc, char **argv, rtapp_options_t *opts)
 							opts->threads_data,
 							(opts->nthreads+1) * \
 							sizeof(thread_data_t));
+					opts->threads_data[opts->nthreads+1].phases_data =
+							malloc(sizeof(rtapp_tasks_phases_t));
 				}
 				parse_thread_args(optarg, opts->nthreads,
 						&opts->threads_data[opts->nthreads],

src/rt-app_types.h
@@ -98,6 +98,14 @@ typedef struct _rtapp_tasks_resource_list_t {
 	struct _rtapp_resource_access_list_t *acl;
 } rtapp_tasks_resource_list_t;
 
+typedef struct _phase_data_t {
+	struct timespec min_et;
+	struct timespec period, deadline;
+	int loop, sleep;
+	rtapp_tasks_resource_list_t *blockages;
+	int nblockages;
+} phase_data_t;
+
 typedef struct _thread_data_t {
 	int ind;
 	char *name;
@@ -106,18 +114,17 @@ typedef struct _thread_data_t {
 	cpu_set_t *cpuset;
 	char *cpuset_str;
 	unsigned long wait_before_start;
-	struct timespec min_et, max_et;
-	struct timespec period, deadline;
+
+	int nphases;
+	phase_data_t *phases_data;
+	int loop;
+
 	struct timespec main_app_start;
-	int loop, sleep;
 
 	FILE *log_handler;
 	policy_t sched_policy;
 	char sched_policy_descr[RTAPP_POLICY_DESCR_LENGTH];
 	int sched_prio;
-
-	rtapp_tasks_resource_list_t *blockages;
-	int nblockages;
 
 #ifdef DLSCHED
 	struct sched_attr dl_params;

commit 7cf363a01e103eea79b04a4d23c127eae1e601bc

rt-app: Fix broken 'loop' default value
According to documentation the default value of 'loop' is -1, but the
threads without a loop field runs only once and stops.

Fix it.

Fixes: 3bda96e6e664 ("add phase feature")
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Acked-by: Vincent Guittot <vincent.guittot@linaro.org>

src/rt-app_parse_config.c
@@ -810,7 +810,7 @@ parse_thread_data(char *name, struct json_object *obj, int index,
 		data->phases = malloc(sizeof(phase_data_t) * data->nphases);
 		parse_thread_phase_data(obj,  &data->phases[0], opts, (long)data);
 		/* Get loop number */
-		data->loop = 1;
+		data->loop = -1;
 	}
 
 }