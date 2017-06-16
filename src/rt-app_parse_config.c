40df85406950e74356ecdcd569e6d97bf112c65d
Colin Ian King <colin.king@canonical.com>
2017-06-16 20:48:05 +0100

    Use correct printf format specifier for an unsigned int
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/rt-app_parse_config.c
+++ src/rt-app_parse_config.c
@@ -667,7 +667,7 @@ static void parse_cpuset_data(struct json_object *obj, cpuset_data_t *data)
 			cpu = json_object_array_get_idx(cpuset_obj, i);
 			cpu_idx = json_object_get_int(cpu);
 			if (cpu_idx > max_cpu) {
-				log_critical(PIN2 "Invalid cpu %d in cpuset %s", cpu_idx, data->cpuset_str);
+				log_critical(PIN2 "Invalid cpu %u in cpuset %s", cpu_idx, data->cpuset_str);
 				free(data->cpuset);
 				free(data->cpuset_str);
 				exit(EXIT_INV_CONFIG);
