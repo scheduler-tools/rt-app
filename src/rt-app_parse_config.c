scheduler-tools/rt-app

commit 6d5adee210d9e9b121b2395db064e99b58678a5b

taskgroups: Policy/taskgroup dependency check
A task (thread) which specifies a taskgroup should not run in a
scheduling policy other than SCHED_OTHER. Add a check for that.
thread_data_t::curr_sched_data (scheduling policy) and
thread_data_t::curr_taskgroup_data are used for that.

Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

src/rt-app_parse_config.c
@@ -850,6 +850,28 @@ static taskgroup_data_t *parse_taskgroup_data(struct json_object *obj)
 	return tg;
 }
 
+static void check_taskgroup_policy_dep(phase_data_t *pdata, thread_data_t *tdata)
+{
+	/* Save sched_data as thread's current sched_data. */
+	if (pdata->sched_data)
+		tdata->curr_sched_data = pdata->sched_data;
+
+	/* Save taskgroup_data as thread's current taskgroup_data. */
+	if (pdata->taskgroup_data)
+		tdata->curr_taskgroup_data = pdata->taskgroup_data;
+
+	/*
+	 * Detect policy/taskgroup misconfiguration: a task which specifies a
+	 * taskgroup should not run in a policy other than SCHED_OTHER.
+	 */
+	if (tdata->curr_sched_data && tdata->curr_sched_data->policy != other &&
+	    tdata->curr_taskgroup_data) {
+		log_critical(PIN2 "No taskgroup support for policy %s",
+			     policy_to_string(tdata->curr_sched_data->policy));
+		exit(EXIT_INV_CONFIG);
+	}
+}
+
 static void
 parse_task_phase_data(struct json_object *obj,
 		  phase_data_t *data, thread_data_t *tdata, rtapp_options_t *opts)
@@ -930,15 +952,19 @@ parse_task_data(char *name, struct json_object *obj, int index,
 	data->def_cpu_data.cpuset = NULL;
 	data->def_cpu_data.cpuset_str = NULL;
 
-	data->curr_sched_data = NULL;
-	data->curr_taskgroup_data = NULL;
-
 	/* cpuset */
 	parse_cpuset_data(obj, &data->cpu_data);
 	/* Scheduling policy */
 	data->sched_data = parse_sched_data(obj, opts->policy);
 	/* Taskgroup */
 	data->taskgroup_data = parse_taskgroup_data(obj);
+
+	/*
+	 * Thread's current sched_data and taskgroup_data are used to detect
+	 * policy/taskgroup misconfiguration.
+	 */
+	data->curr_sched_data = data->sched_data;
+	data->curr_taskgroup_data = data->taskgroup_data;
 
 	/* initial delay */
 	data->delay = get_int_value_from(obj, "delay", TRUE, 0);
@@ -968,6 +994,11 @@ parse_task_data(char *name, struct json_object *obj, int index,
 			log_info(PIN "Parsing phase %s", key);
 			assure_type_is(val, phases_obj, key, json_type_object);
 			parse_task_phase_data(val, &data->phases[idx], data, opts);
+			/*
+			 * Uses thread's current sched_data and taskgroup_data
+			 * to detect policy/taskgroup misconfiguration.
+			 */
+			check_taskgroup_policy_dep(&data->phases[idx], data);
 		}
 
 		/* Get loop number */
@@ -987,6 +1018,11 @@ parse_task_data(char *name, struct json_object *obj, int index,
 		data->phases[0].sched_data = NULL;
 
 		/*
+		 * Uses thread's current sched_data and taskgroup_data
+		 * to detect policy/taskgroup misconfiguration.
+		 */
+		check_taskgroup_policy_dep(&data->phases[0], data);
+		/*
 		 * Get loop number:
 		 *
 		 * If loop is specified, we already parsed it in
@@ -1002,6 +1038,9 @@ parse_task_data(char *name, struct json_object *obj, int index,
 			data->loop = -1;
 	}
 
+	/* Reset thread's current sched_data and taskgroup_data after parsing. */
+	data->curr_sched_data = NULL;
+	data->curr_taskgroup_data = NULL;
 }
 
 static void

commit 7e9d5bfb98cb3fe0448d37902d34ba62d1f3e60f

taskgroups: Fix policy/taskgroup dependency check for non-specified policy
parse_sched_data() returns a new sched_data_t if at least one new
scheduler parameter (thread priority, deadline or uclamp change) was
found. In case the scheduler policy wasn't explicitly specified for
that phase object its sched_data_t policy is set to -1.

check_taskgroup_policy_dep() should not save this sched_data_t as the
curr_sched_data pointer for the phase_data_t object but keep the
existing one for the policy/taskgroup dependency check.

Fixes: 6d5adee210d9 ("taskgroups: Policy/taskgroup dependency check")

Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

src/rt-app_parse_config.c
@@ -939,8 +939,11 @@ static taskgroup_data_t *parse_taskgroup_data(struct json_object *obj)
 
 static void check_taskgroup_policy_dep(phase_data_t *pdata, thread_data_t *tdata)
 {
-	/* Save sched_data as thread's current sched_data. */
-	if (pdata->sched_data)
+	/*
+	 * Save sched_data as thread's current sched_data in case its policy_t is
+	 * set to a valid scheduler policy.
+	 */
+	if (pdata->sched_data && pdata->sched_data->policy != -1)
 		tdata->curr_sched_data = pdata->sched_data;
 
 	/* Save taskgroup_data as thread's current taskgroup_data. */