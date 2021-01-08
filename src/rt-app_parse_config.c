scheduler-tools/rt-app

commit 6d5adee210d9e9b121b2395db064e99b58678a5b

taskgroups: Policy/taskgroup dependency check
A task (thread) which specifies a taskgroup should not run in a
scheduling policy other than SCHED_OTHER. Add a check for that.
thread_data_t::curr_sched_data (scheduling policy) and
thread_data_t::curr_taskgroup_data are used for that.

Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

src/rt-app_parse_config.c
@@ -850,6 +850,28 @@ static taskgroup_data_t *parse_taskgroup_data(struct json_object *obj)
 	return tg;
 }
 
+static void check_taskgroup_policy_dep(phase_data_t *pdata, thread_data_t *tdata)
+{
+	/* Save sched_data as thread's current sched_data. */
+	if (pdata->sched_data)
+		tdata->curr_sched_data = pdata->sched_data;
+
+	/* Save taskgroup_data as thread's current taskgroup_data. */
+	if (pdata->taskgroup_data)
+		tdata->curr_taskgroup_data = pdata->taskgroup_data;
+
+	/*
+	 * Detect policy/taskgroup misconfiguration: a task which specifies a
+	 * taskgroup should not run in a policy other than SCHED_OTHER.
+	 */
+	if (tdata->curr_sched_data && tdata->curr_sched_data->policy != other &&
+	    tdata->curr_taskgroup_data) {
+		log_critical(PIN2 "No taskgroup support for policy %s",
+			     policy_to_string(tdata->curr_sched_data->policy));
+		exit(EXIT_INV_CONFIG);
+	}
+}
+
 static void
 parse_task_phase_data(struct json_object *obj,
 		  phase_data_t *data, thread_data_t *tdata, rtapp_options_t *opts)
@@ -930,15 +952,19 @@ parse_task_data(char *name, struct json_object *obj, int index,
 	data->def_cpu_data.cpuset = NULL;
 	data->def_cpu_data.cpuset_str = NULL;
 
-	data->curr_sched_data = NULL;
-	data->curr_taskgroup_data = NULL;
-
 	/* cpuset */
 	parse_cpuset_data(obj, &data->cpu_data);
 	/* Scheduling policy */
 	data->sched_data = parse_sched_data(obj, opts->policy);
 	/* Taskgroup */
 	data->taskgroup_data = parse_taskgroup_data(obj);
+
+	/*
+	 * Thread's current sched_data and taskgroup_data are used to detect
+	 * policy/taskgroup misconfiguration.
+	 */
+	data->curr_sched_data = data->sched_data;
+	data->curr_taskgroup_data = data->taskgroup_data;
 
 	/* initial delay */
 	data->delay = get_int_value_from(obj, "delay", TRUE, 0);
@@ -968,6 +994,11 @@ parse_task_data(char *name, struct json_object *obj, int index,
 			log_info(PIN "Parsing phase %s", key);
 			assure_type_is(val, phases_obj, key, json_type_object);
 			parse_task_phase_data(val, &data->phases[idx], data, opts);
+			/*
+			 * Uses thread's current sched_data and taskgroup_data
+			 * to detect policy/taskgroup misconfiguration.
+			 */
+			check_taskgroup_policy_dep(&data->phases[idx], data);
 		}
 
 		/* Get loop number */
@@ -987,6 +1018,11 @@ parse_task_data(char *name, struct json_object *obj, int index,
 		data->phases[0].sched_data = NULL;
 
 		/*
+		 * Uses thread's current sched_data and taskgroup_data
+		 * to detect policy/taskgroup misconfiguration.
+		 */
+		check_taskgroup_policy_dep(&data->phases[0], data);
+		/*
 		 * Get loop number:
 		 *
 		 * If loop is specified, we already parsed it in
@@ -1002,6 +1038,9 @@ parse_task_data(char *name, struct json_object *obj, int index,
 			data->loop = -1;
 	}
 
+	/* Reset thread's current sched_data and taskgroup_data after parsing. */
+	data->curr_sched_data = NULL;
+	data->curr_taskgroup_data = NULL;
 }
 
 static void

commit dfbc1e73d33fb2c2b9c1e2481bd0e333502bdd3e

taskgroups: Fix SCHED_IDLE policy support
Currently rt-app prevents a task with SCHED_IDLE policy to run in a
taskgroup:

  # rt-app foo.json
  [rt-app] <crit> [json] No taskgroup support for policy SCHED_IDLE

Adapt the rt-app parser code to allow this.

There is no SCHED_BATCH support in rt-app yet, i.e. this policy doesn't
have to be considered here.

Fixes: 6d5adee210d9 ("taskgroups: Policy/taskgroup dependency check")

Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

src/rt-app_parse_config.c
@@ -949,13 +949,17 @@ static void check_taskgroup_policy_dep(phase_data_t *pdata, thread_data_t *tdata
 
 	/*
 	 * Detect policy/taskgroup misconfiguration: a task which specifies a
-	 * taskgroup should not run in a policy other than SCHED_OTHER.
+	 * taskgroup should not run in a policy other than SCHED_OTHER or
+	 * SCHED_IDLE.
 	 */
-	if (tdata->curr_sched_data && tdata->curr_sched_data->policy != other &&
-	    tdata->curr_taskgroup_data) {
-		log_critical(PIN2 "No taskgroup support for policy %s",
-			     policy_to_string(tdata->curr_sched_data->policy));
-		exit(EXIT_INV_CONFIG);
+	if (tdata->curr_sched_data && tdata->curr_taskgroup_data) {
+		policy_t policy = tdata->curr_sched_data->policy;
+
+		if (policy != other && policy != idle) {
+			log_critical(PIN2 "No taskgroup support for policy %s",
+			             policy_to_string(policy));
+			exit(EXIT_INV_CONFIG);
+		}
 	}
 }
 