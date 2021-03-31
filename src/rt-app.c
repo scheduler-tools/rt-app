5b74a66514861a799898af2ede86db9d9bc8b01e
Douglas RAILLARD <douglas.raillard@gmail.com>
2021-03-31 11:00:12 +0100

    src: Fix incorrect use of cpu_set_t
    cpu_set_t allocated dynamically with CPU_ALLOC need to be manipulated
    with CPU_*_S() family of macro, otherwise it will be assumed that the
    size is equal to sizeof(cpu_set_t), leading to undefined behaviours.

--- src/rt-app.c
+++ src/rt-app.c
@@ -793,7 +793,7 @@ static int create_cpuset_str(cpuset_data_t *cpu_data)
 	for (i = 0; i < 10000 && cpu_count; ++i) {
 		unsigned int n;
 
-		if (CPU_ISSET(i, cpu_data->cpuset)) {
+		if (CPU_ISSET_S(cpu_data->cpusetsize, i, cpu_data->cpuset)) {
 			--cpu_count;
 			if (size_needed <= (idx + 1)) {
 				log_error("Not enough memory for array");
@@ -861,8 +861,12 @@ static void set_thread_affinity(thread_data_t *data, cpuset_data_t *cpu_data)
 	if (actual_cpu_data->cpuset == NULL)
 		actual_cpu_data = &data->def_cpu_data;
 
-	if (!CPU_EQUAL(actual_cpu_data->cpuset, data->curr_cpu_data->cpuset))
-	{
+	if (
+	    actual_cpu_data->cpusetsize != data->curr_cpu_data->cpusetsize ||
+	    !CPU_EQUAL_S(
+			 actual_cpu_data->cpusetsize,
+			 actual_cpu_data->cpuset, data->curr_cpu_data->cpuset))
+	  {
 		log_debug("[%d] setting cpu affinity to CPU(s) %s", data->ind,
 			actual_cpu_data->cpuset_str);
 		ret = pthread_setaffinity_np(pthread_self(),
