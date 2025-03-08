e6919ab93cde57ff0c4fc1226ee2d3e0af53f19e
Aurelien Jarno <aurelien@aurel32.net>
2025-03-08 17:50:47 +0100

    Fix build with GNU libc 2.41
    Starting with GNU libc 2.41, the sched_getattr and sched_setattr
    functions, and the sched_attr structure are provided in sched.h. This
    causes a conflict with the definition in libdl.
    
    The configure script already checks for the sched_setattr function, but
    then the logic to compile and link libdl was wrong, as it is not
    possible to directly check the HAVE_SCHED_SETATTR with AM_CONDITIONAL.
    In addition the dl_syscalls.h include file needs to be guarded, and a
    few include files that were indirectly included by this file not to be
    done explictly.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- src/rt-app_parse_config.c
+++ src/rt-app_parse_config.c
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <string.h>
 #include <stdbool.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <json-c/json.h>
 
 #include "rt-app_utils.h"
