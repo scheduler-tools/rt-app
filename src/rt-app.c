scheduler-tools/rt-app

commit 395ec42c28c1e7dbcd08904903c01936ef7f7e27

rt-app: add per phase set priority event
Add capability to change task's priority when starting a new phase. The
behavior is similar to events. a change only happen when value are defined
in the phase otherwise the scheduling parameters stay unchanged.

All 4 scheduling classes are supported:
-Other
-Fifo
-RR
-deadline

We can change scheduling class, priority or deadline parameters.
The file tutorial.txt has been updated to describe this new capability and
the common properties of threads and phases have also been reordered a bit.

Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

src/rt-app.c
@@ -590,6 +590,111 @@ static void set_thread_affinity(thread_data_t *data, cpuset_data_t *cpu_data)
 	}
 }
 
+static void set_thread_priority(thread_data_t *data, sched_data_t *sched_data)
+{
+	struct sched_param param;
+	policy_t policy;
+#ifdef DLSCHED
+	struct sched_attr dl_params;
+	pid_t tid;
+	unsigned int flags = 0;
+#endif
+	int ret;
+
+	if (sched_data == NULL)
+		return;
+
+	if (data->curr_sched_data == sched_data)
+		return;
+
+	if ((sched_data->policy == same) &&  (data->curr_sched_data)){
+		/* if policy not specificed, reuse previous policy */
+		sched_data->policy = data->curr_sched_data->policy;
+	}
+
+
+	switch (sched_data->policy)
+	{
+		case rr:
+		case fifo:
+			log_debug("[%d] setting scheduler %s priority %d", data->ind,
+					policy_to_string(sched_data->policy), sched_data->prio);
+			param.sched_priority = sched_data->prio;
+			ret = pthread_setschedparam(pthread_self(),
+					sched_data->policy,
+					&param);
+			if (ret != 0) {
+				log_critical("[%d] pthread_setschedparam"
+				     "returned %d", data->ind, ret);
+				errno = ret;
+				perror("pthread_setschedparam");
+				exit(EXIT_FAILURE);
+			}
+			break;
+
+		case other:
+			log_debug("[%d] setting scheduler %s priority %d", data->ind,
+					policy_to_string(sched_data->policy), sched_data->prio);
+
+
+			if (sched_data->prio > 19 || sched_data->prio < -20) {
+				log_critical("[%d] setpriority "
+					"%d nice invalid. "
+					"Valid between -20 and 19",
+					data->ind, sched_data->prio);
+				exit(EXIT_FAILURE);
+			}
+
+			ret = setpriority(PRIO_PROCESS, 0,
+					sched_data->prio);
+			if (ret != 0) {
+				log_critical("[%d] setpriority"
+				     "returned %d", data->ind, ret);
+				errno = ret;
+				perror("setpriority");
+				exit(EXIT_FAILURE);
+			}
+
+			data->lock_pages = 0; /* forced off for SCHED_OTHER */
+			break;
+
+#ifdef DLSCHED
+		case deadline:
+			log_debug("[%d] setting scheduler %s exec %lu, deadline %lu"
+					" period %lu", data->ind,
+					policy_to_string(sched_data->policy), sched_data->period,
+					sched_data->runtime, sched_data->deadline);
+
+			tid = gettid();
+			dl_params.size = sizeof(struct sched_attr);
+			dl_params.sched_flags = 0;
+			dl_params.sched_policy = SCHED_DEADLINE;
+			dl_params.sched_priority = 0;
+			dl_params.sched_runtime = sched_data->runtime;
+			dl_params.sched_deadline = sched_data->deadline;
+			dl_params.sched_period = sched_data->period;
+
+			ret = sched_setattr(tid, &dl_params, flags);
+			if (ret != 0) {
+				log_critical("[%d] sched_setattr "
+						"returned %d", data->ind, ret);
+				errno = ret;
+				perror("sched_setattr");
+				exit(EXIT_FAILURE);
+			}
+		break;
+#endif
+
+		default:
+			log_error("Unknown scheduling policy %d",
+					sched_data->policy);
+
+			exit(EXIT_FAILURE);
+	}
+
+	data->curr_sched_data = sched_data;
+}
+
 void *thread_body(void *arg)
 {
 	thread_data_t *data = (thread_data_t*) arg;
@@ -603,9 +708,7 @@ void *thread_body(void *arg)
 	timing_point_t *timings;
 	timing_point_t tmp_timing;
 	unsigned int timings_size, timing_loop;
-	pid_t tid;
 	struct sched_attr attr;
-	unsigned int flags = 0;
 	int ret, phase, phase_loop, thread_loop, log_idx;
 
 	/* Set thread name */
@@ -617,75 +720,7 @@ void *thread_body(void *arg)
 	/* Get the 1st phase's data */
 	pdata = &data->phases[0];
 
-	/* Set scheduling policy and print pretty info on stdout */
-	log_notice("[%d] Using %s policy with priority %d", data->ind, data->sched_policy_descr, data->sched_prio);
-	switch (data->sched_policy)
-	{
-		case rr:
-		case fifo:
-			fprintf(data->log_handler, "# Policy : %s priority : %d\n",
-					(data->sched_policy == rr ? "SCHED_RR" : "SCHED_FIFO"), data->sched_prio);
-			param.sched_priority = data->sched_prio;
-			ret = pthread_setschedparam(pthread_self(),
-					data->sched_policy,
-					&param);
-			if (ret != 0) {
-				errno = ret;
-				perror("pthread_setschedparam");
-				exit(EXIT_FAILURE);
-			}
-			break;
-
-		case other:
-			fprintf(data->log_handler, "# Policy : SCHED_OTHER priority : %d\n", data->sched_prio);
-
-			if (data->sched_prio > 19 || data->sched_prio < -20) {
-				log_critical("[%d] setpriority "
-					"%d nice invalid. "
-					"Valid between -20 and 19",
-					data->ind, data->sched_prio);
-				exit(EXIT_FAILURE);
-			}
-
-			if (data->sched_prio) {
-				ret = setpriority(PRIO_PROCESS, 0,
-						data->sched_prio);
-				if (ret != 0) {
-					log_critical("[%d] setpriority"
-					     "returned %d", data->ind, ret);
-					errno = ret;
-					perror("setpriority");
-					exit(EXIT_FAILURE);
-				}
-			}
-
-			data->lock_pages = 0; /* forced off for SCHED_OTHER */
-			break;
-
-#ifdef DLSCHED
-		case deadline:
-			fprintf(data->log_handler, "# Policy : SCHED_DEADLINE\n");
-			tid = gettid();
-			attr.size = sizeof(attr);
-			attr.sched_flags = 0;
-			attr.sched_policy = SCHED_DEADLINE;
-			attr.sched_priority = 0;
-			attr.sched_runtime = data->runtime;
-			attr.sched_deadline = data->deadline; 
-			attr.sched_period = data->period;
-
-			log_notice("[%d] period: %lu, exec: %lu, deadline: %lu",
-				data->ind, data->period, data->runtime, data->deadline);
-			break;
-#endif
-
-		default:
-			log_error("Unknown scheduling policy %d",
-					data->sched_policy);
-
-			exit(EXIT_FAILURE);
-	}
-
+	/* Init timing buffer */
 	if (opts.logsize > 0) {
 		timings = malloc(opts.logsize);
 		timings_size = opts.logsize / sizeof(timing_point_t);
@@ -732,24 +767,6 @@ void *thread_body(void *arg)
 	if (opts.ftrace)
 		log_ftrace(ft_data.marker_fd, "[%d] starts", data->ind);
 
-#ifdef DLSCHED
-	/* TODO find a better way to handle that constraint */
-	/*
-	 * Set the task to SCHED_DEADLINE as far as possible touching its
-	 * budget as little as possible for the first iteration.
-	 */
-	if (data->sched_policy == SCHED_DEADLINE) {
-		ret = sched_setattr(tid, &attr, flags);
-		if (ret != 0) {
-			log_critical("[%d] sched_setattr "
-				     "returned %d", data->ind, ret);
-			errno = ret;
-			perror("sched_setattr");
-			exit(EXIT_FAILURE);
-		}
-	}
-#endif
-
 	if (data->delay > 0) {
 		struct timespec delay = usec_to_timespec(data->delay);
 
@@ -758,6 +775,17 @@ void *thread_body(void *arg)
 		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t_first,
 				NULL);
 	}
+
+	/* TODO find a better way to handle that constraint:
+	 * Set the task to SCHED_DEADLINE as far as possible touching its
+	 * budget as little as possible for the first iteration.
+	 */
+
+	/* Set scheduling policy and print pretty info on stdout */
+	log_notice("[%d] Starting with %s policy with priority %d",
+			data->ind, policy_to_string(data->sched_data->policy),
+			data->sched_data->prio);
+	set_thread_priority(data, data->sched_data);
 
 	/*
 	 * phase        - index of current phase in data->phases array
@@ -774,6 +802,7 @@ void *thread_body(void *arg)
 		struct timespec t_diff, t_rel_start;
 
 		set_thread_affinity(data, &pdata->cpu_data);
+		set_thread_priority(data, pdata->sched_data);
 
 		if (opts.ftrace)
 			log_ftrace(ft_data.marker_fd,
@@ -987,7 +1016,7 @@ int main(int argc, char* argv[])
 				opts.logbasename, opts.threads_data[i].name,
 				opts.threads_data[i].ind,
 				opts.threads_data[i].name,
-				opts.threads_data[i].sched_policy_descr);
+				policy_to_string(opts.threads_data[i].sched_data->policy));
 
 			if ( i == nthreads-1)
 				fprintf(gnuplot_script, "\n");
@@ -1021,7 +1050,7 @@ int main(int argc, char* argv[])
 				opts.logbasename, opts.threads_data[i].name,
 				opts.threads_data[i].ind,
 				opts.threads_data[i].name,
-				opts.threads_data[i].sched_policy_descr);
+				policy_to_string(opts.threads_data[i].sched_data->policy));
 
 			if ( i == nthreads-1)
 				fprintf(gnuplot_script, "\n");

src/rt-app_parse_config.c
@@ -683,6 +683,74 @@ static void parse_cpuset_data(struct json_object *obj, cpuset_data_t *data)
 	log_info(PIN "key: cpus %s", data->cpuset_str);
 }
 
+static sched_data_t *parse_sched_data(struct json_object *obj, int def_policy)
+{
+	sched_data_t tmp_data;
+	char *def_str_policy;
+	char *policy;
+	int prior_def = -1;
+
+	/* Get default policy */
+	def_str_policy = policy_to_string(def_policy);
+
+	/* Get Policy */
+	policy = get_string_value_from(obj, "policy", TRUE, def_str_policy);
+	if (policy ){
+		if (string_to_policy(policy, &tmp_data.policy) != 0) {
+			log_critical(PIN2 "Invalid policy %s", policy);
+			exit(EXIT_INV_CONFIG);
+		}
+	} else {
+		tmp_data.policy = -1;
+	}
+
+	/* Get priority */
+	if (tmp_data.policy == -1)
+		prior_def = -1;
+	else if (tmp_data.policy == other)
+		prior_def = DEFAULT_THREAD_NICE;
+	else
+		prior_def = DEFAULT_THREAD_PRIORITY;
+
+	tmp_data.prio = get_int_value_from(obj, "priority", TRUE, prior_def);
+
+	/* deadline params */
+	tmp_data.runtime = get_int_value_from(obj, "dl-runtime", TRUE, 0);
+	tmp_data.period = get_int_value_from(obj, "dl-period", TRUE, tmp_data.runtime);
+	tmp_data.deadline = get_int_value_from(obj, "dl-deadline", TRUE, tmp_data.period);
+
+
+	if (def_policy != -1) {
+		/* Support legacy grammar for thread object */
+		if (!tmp_data.runtime)
+			tmp_data.runtime = get_int_value_from(obj, "runtime", TRUE, 0);
+		if (!tmp_data.period)
+			tmp_data.period = get_int_value_from(obj, "period", TRUE, tmp_data.runtime);
+		if (!tmp_data.deadline)
+			tmp_data.deadline = get_int_value_from(obj, "deadline", TRUE, tmp_data.period);
+	}
+
+	/* Move from usec to nanosec */
+	tmp_data.runtime *= 1000;
+	tmp_data.period *= 1000;
+	tmp_data.deadline *= 1000;
+
+	/* Check if we found at least one meaningful scheduler parameter */
+	if (tmp_data.prio != -1 || tmp_data.runtime || tmp_data.period || tmp_data.period) {
+		sched_data_t *new_data;
+
+		/* At least 1 parameters has been set in the object */
+		new_data = malloc(sizeof(sched_data_t));
+		memcpy( new_data, &tmp_data,sizeof(sched_data_t));
+
+		log_debug(PIN "key: set scheduler %d with priority %d", new_data->policy, new_data->prio);
+
+		return new_data;
+	}
+
+	return NULL;
+}
+
 static void
 parse_thread_phase_data(struct json_object *obj,
 		  phase_data_t *data, rtapp_options_t *opts, long tag)
@@ -717,16 +785,15 @@ parse_thread_phase_data(struct json_object *obj,
 		}
 	}
 	parse_cpuset_data(obj, &data->cpu_data);
+	data->sched_data = parse_sched_data(obj, -1);
+
 }
 
 static void
 parse_thread_data(char *name, struct json_object *obj, int index,
 		  thread_data_t *data, rtapp_options_t *opts)
 {
-	char *policy;
-	char def_policy[RTAPP_POLICY_DESCR_LENGTH];
 	struct json_object *phases_obj, *resources;
-	int prior_def;
 
 	log_info(PFX "Parsing thread %s [%d]", name, index);
 
@@ -735,45 +802,19 @@ parse_thread_data(char *name, struct json_object *obj, int index,
 	data->ind = index;
 	data->name = strdup(name);
 	data->lock_pages = opts->lock_pages;
+
 	data->cpu_data.cpuset = NULL;
 	data->cpu_data.cpuset_str = NULL;
 	data->curr_cpu_data = NULL;
 	data->def_cpu_data.cpuset = NULL;
 	data->def_cpu_data.cpuset_str = NULL;
 
-	/* policy */
-	policy_to_string(opts->policy, def_policy);
-	policy = get_string_value_from(obj, "policy", TRUE, def_policy);
-	if (policy) {
-		if (string_to_policy(policy, &data->sched_policy) != 0) {
-			log_critical(PIN2 "Invalid policy %s", policy);
-			exit(EXIT_INV_CONFIG);
-		}
-	}
-	policy_to_string(data->sched_policy, data->sched_policy_descr);
-
-	/* priority */
-	if (data->sched_policy == other)
-		prior_def = DEFAULT_THREAD_NICE;
-	else
-		prior_def = DEFAULT_THREAD_PRIORITY;
-
-	data->sched_prio = get_int_value_from(obj, "priority", TRUE,
-				 prior_def);
-
-	/* deadline params */
-	data->runtime = get_int_value_from(obj, "dl-runtime", TRUE, 0) * 1000;
-	if (!data->runtime)
-		data->runtime = get_int_value_from(obj, "runtime", TRUE, 0);
-	data->period = get_int_value_from(obj, "dl-period", TRUE, 0) * 1000;
-	if (!data->period)
-		data->period = get_int_value_from(obj, "period", TRUE, data->runtime);
-	data->deadline = get_int_value_from(obj, "dl-deadline", TRUE, 0) * 1000;
-	if (!data->period)
-		data->deadline = get_int_value_from(obj, "deadline", TRUE, data->period);
+	data->curr_sched_data = NULL;
 
 	/* cpuset */
 	parse_cpuset_data(obj, &data->cpu_data);
+	/* Scheduling policy */
+	data->sched_data = parse_sched_data(obj, opts->policy);
 
 	/* initial delay */
 	data->delay = get_int_value_from(obj, "delay", TRUE, 0);
@@ -810,6 +851,14 @@ parse_thread_data(char *name, struct json_object *obj, int index,
 		data->nphases = 1;
 		data->phases = malloc(sizeof(phase_data_t) * data->nphases);
 		parse_thread_phase_data(obj,  &data->phases[0], opts, (long)data);
+
+		/* There is no "phases" object which means that thread and phase will
+		 * use same scheduling parameters. But thread object looks for default
+		 * value when parameters are not defined whereas phase doesn't.
+		 * We remove phase's scheduling policy which is a subset of thread's one
+		 */
+		free(data->phases[0].sched_data);
+		data->phases[0].sched_data = NULL;
 
 		/*
 		 * Get loop number:

src/rt-app_types.h
@@ -50,6 +50,7 @@ typedef enum policy_t
 #ifdef DLSCHED
 	, deadline = SCHED_DEADLINE
 #endif
+	, same = -1
 } policy_t;
 
 typedef enum resource_t
@@ -147,11 +148,21 @@ typedef struct _cpuset_data_t {
 	size_t cpusetsize;
 } cpuset_data_t;
 
+typedef struct _sched_data_t {
+	policy_t policy;
+	int prio;
+	unsigned long runtime;
+	unsigned long deadline;
+	unsigned long period;
+} sched_data_t;
+
 typedef struct _phase_data_t {
 	int loop;
 	event_data_t *events;
 	int nbevents;
+	int sched_prio;
 	cpuset_data_t cpu_data;
+	sched_data_t *sched_data;
 } phase_data_t;
 
 typedef struct _thread_data_t {
@@ -160,11 +171,13 @@ typedef struct _thread_data_t {
 	int lock_pages;
 	int duration;
 	rtapp_resource_t **resources;
+
 	cpuset_data_t cpu_data; /* cpu set information */
 	cpuset_data_t *curr_cpu_data; /* Current cpu set being used */
 	cpuset_data_t def_cpu_data; /* Default cpu set for task */
 
-	unsigned long runtime, deadline, period;
+	sched_data_t *sched_data; /* scheduler policy information */
+	sched_data_t *curr_sched_data; /* current scheduler policy */
 
 	int loop;
 	int nphases;
@@ -173,15 +186,8 @@ typedef struct _thread_data_t {
 	struct timespec main_app_start;
 
 	FILE *log_handler;
-	policy_t sched_policy;
-	char sched_policy_descr[RTAPP_POLICY_DESCR_LENGTH];
-	int sched_prio;
 
 	unsigned long delay;
-
-#ifdef DLSCHED
-	struct sched_attr dl_params;
-#endif
 } thread_data_t;
 
 typedef struct _ftrace_data_t {

src/rt-app_utils.c
@@ -185,28 +185,24 @@ string_to_policy(const char *policy_name, policy_t *policy)
 	return 0;
 }
 
-int
-policy_to_string(policy_t policy, char *policy_name)
+char *
+policy_to_string(policy_t policy)
 {
 	switch (policy) {
 		case other:
-			strcpy(policy_name, "SCHED_OTHER");
-			break;
+			return "SCHED_OTHER";
 		case rr:
-			strcpy(policy_name, "SCHED_RR");
-			break;
+			return "SCHED_RR";
 		case fifo:
-			strcpy(policy_name, "SCHED_FIFO");
-			break;
+			return "SCHED_FIFO";
 #ifdef DLSCHED
 		case deadline:
-			strcpy(policy_name, "SCHED_DEADLINE");
-			break;
+			return "SCHED_DEADLINE";
 #endif
 		default:
-			return 1;
+			return NULL;
 	}
-	return 0;
+	return NULL;
 }
 
 int

src/rt-app_utils.h
@@ -122,8 +122,8 @@ unsigned long long
 timespec_to_nsec(struct timespec *ts);
 #endif
 
-int
-policy_to_string(policy_t policy, char *policy_name);
+char *
+policy_to_string(policy_t policy);
 
 int
 string_to_policy(const char *policy_name, policy_t *policy);

commit 5890dd37cacc7693a2790d833ce38ebf78835055

rt-app: Reorder page locking to properly skip it for SCHED_OTHER/IDLE
As page locking was done very early during thread setup, its disabling
for SCHED_OTHER and SCHED_IDLE in set_thread_param had no impact, and
you still had to disable locking globally for workloads only consisting
of such threads.

Fixes: 395ec42c28c1 ("rt-app: add per phase set priority event")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

src/rt-app.c
@@ -1187,17 +1187,6 @@ void *thread_body(void *arg)
 	}
 	timing_loop = 0;
 
-	/* Lock pages */
-	if (data->lock_pages == 1)
-	{
-		log_notice("[%d] Locking pages in memory", data->ind);
-		ret = mlockall(MCL_CURRENT | MCL_FUTURE);
-		if (ret != 0) {
-			perror("mlockall");
-			exit(EXIT_FAILURE);
-		}
-	}
-
 	if (data->ind == 0) {
 		/*
 		 * Only first thread sets t_zero. Other threads sync with this
@@ -1246,6 +1235,17 @@ void *thread_body(void *arg)
 	set_thread_param(data, data->sched_data);
 	set_thread_membind(data, &data->numa_data);
 	set_thread_taskgroup(data, data->taskgroup_data);
+
+	/* Lock pages */
+	if (data->lock_pages == 1)
+	{
+		log_notice("[%d] Locking pages in memory", data->ind);
+		ret = mlockall(MCL_CURRENT | MCL_FUTURE);
+		if (ret != 0) {
+			perror("mlockall");
+			exit(EXIT_FAILURE);
+		}
+	}
 
 	/*
 	 * phase        - index of current phase in data->phases array